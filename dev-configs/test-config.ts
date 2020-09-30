2cbe7540297388cdd6a96c0379d95771ef6dd805
manh <manh.vv.htth@gmail.com>
2020-09-30 19:29:58 +0700

    refactor: :wrench: remove dev-configs

--- dev-configs/test-config.ts
+++
@@ -1,6 +0,0 @@
-import '../src/shares/loadEnv';
-import { logger } from '../src/shares/logger';
-
-export default () => {
-  logger.debug('configuring test');
-};
---
+++ test-config.ts
@@ -0,0 +1,6 @@
+import './src/shares/loadEnv';
+import { logger } from './src/shares/logger';
+
+export default () => {
+  logger.debug('configuring test');
+};
